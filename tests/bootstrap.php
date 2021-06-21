3748d6c4fab311145bda8bd1d8ac5ed4c5fcc71a
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-06-22 01:00:56 +0300

    update testing repo

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,39 +1,7 @@
 <?php
 
-use Nip\Container\Container;
-
-define('PROJECT_BASE_PATH', __DIR__ . '/..');
-define('TEST_BASE_PATH', __DIR__);
-define('TEST_FIXTURE_PATH', __DIR__ . DIRECTORY_SEPARATOR . 'fixtures');
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-
-if (file_exists(TEST_BASE_PATH . DIRECTORY_SEPARATOR . '.env')) {
-    $dotenv = Dotenv\Dotenv::createImmutable(TEST_BASE_PATH);
-    $dotenv->load();
-}
-
-function envVar($key)
-{
-    if (isset($_ENV[$key])) {
-        return $_ENV[$key];
-    }
-    return getenv($key);
-}
-
-$container = new Container();
-Container::setInstance($container);
-
-$container->set('inflector', \Nip\Inflector\Inflector::instance());
-
-$translator = Mockery::mock(\Nip\I18n\Translator::class)->shouldAllowMockingProtectedMethods()->makePartial();
-$translator->shouldReceive('persistLocale');
-$container->set('translator', $translator);
-$container->set('request', new \Nip\Request());
-
-$data = [
-    'payments' => [
+$configData =  [
         'gateways' => ['Librapay']
-    ]
 ];
-$container->set('config', new \Nip\Config\Config($data));
+
+require './vendor/bytic/payments-tests/src/boostrap/bootstrap.php';
