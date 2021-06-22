9fbf6058bf609c892a8dbc90c6284e9bc7637779
Gabriel Solomon <hello@gabrielsolomon.ro>
2021-06-22 13:44:06 +0300

    update to new env function

--- tests/fixtures/Records/LibrapayMethodData.php
+++ tests/fixtures/Records/LibrapayMethodData.php
@@ -17,7 +17,7 @@ class LibrapayMethodData
     {
         $data = 'a:2:{'
             . 's:15:"payment_gateway";s:8:"librapay";s:8:"librapay";'
-            . 'a:7:{s:7:"sandbox";s:2:"no";s:8:"merchant";s:15:"' . envVar('LIBRAPAY_MERCHANT') . '";s:12:"merchantName";s:36:"' . envVar('LIBRAPAY_MERCHANTNAME') . '";s:13:"merchantEmail";s:17:"' . envVar('LIBRAPAY_MERCHANTEMAIL') . '";s:11:"merchantUrl";s:16:"register.42km.ro";s:8:"terminal";s:8:"' . envVar('LIBRAPAY_TERMINAL') . '";s:3:"key";s:32:"' . envVar('LIBRAPAY_KEY') . '";}}';
+            . 'a:7:{s:7:"sandbox";s:2:"no";s:8:"merchant";s:15:"' . env('LIBRAPAY_MERCHANT') . '";s:12:"merchantName";s:36:"' . env('LIBRAPAY_MERCHANTNAME') . '";s:13:"merchantEmail";s:17:"' . env('LIBRAPAY_MERCHANTEMAIL') . '";s:11:"merchantUrl";s:16:"register.42km.ro";s:8:"terminal";s:8:"' . env('LIBRAPAY_TERMINAL') . '";s:3:"key";s:32:"' . env('LIBRAPAY_KEY') . '";}}';
         return $data;
     }
 
